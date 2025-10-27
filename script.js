fb71c2ae9bd46d1b720fd3b5933970c8205c08f2
tameem akhtar <akhtartamimm@gmail.com>
2025-10-27 22:18:50 +0530

    20 questions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //  Q NUMBER 1
 let t = "ilaichi";
-console.log ("my fvrt food is : " + t)
+console.log("my fvrt food is : " + t)
 
 
 //  Q NUMBER 2
@@ -15,29 +15,29 @@ console.log(age);
 
 
 let string = "i love javascript programming";
- let tameem = string.slice(7,18)
+let tameem = string.slice(7, 18)
 console.log(tameem);
 
 //  Q NUMBER 4
 
 let football = "football is my favourite sports";
 console.log(football)
- let cricket = football.replace("football", "cricket");
-console.log (cricket);
+let cricket = football.replace("football", "cricket");
+console.log(cricket);
 
 //  Q NUMBER 5
 
 
 let world = "hello world , world is beatifull";
-console.log (world);
-let replace = world.replaceAll ( "world" , "earth");
-console.log (replace);
+console.log(world);
+let replace = world.replaceAll("world", "earth");
+console.log(replace);
 
 
 //  Q NUMBER 6
 
 let fruits = "apple,banana, grapes,mango";
-let fruitss= fruits.split() ;
+let fruitss = fruits.split();
 console.log(fruitss);
 
 
@@ -45,16 +45,16 @@ console.log(fruitss);
 
 let number1 = 99;
 let number2 = 101;
-let adding = (number1+number2);
+let adding = (number1 + number2);
 console.log(adding);
 
-let minuse = (number1-number2);
+let minuse = (number1 - number2);
 console.log(minuse);
 
-let multiply = (number1*number2);
+let multiply = (number1 * number2);
 console.log(multiply);
 
-let divide = (number1/number2);
+let divide = (number1 / number2);
 console.log(divide);
 
 
@@ -62,31 +62,109 @@ console.log(divide);
 //  Q NUMBER 8
 
 let num = 99;
-console.log(typeof(num));
+console.log(typeof (num));
 
 let str = "tameem";
-console.log(typeof(str));
+console.log(typeof (str));
 
 let bool = true;
-console.log(typeof(bool));
+console.log(typeof (bool));
 
 let nul = null;
-console.log(typeof(nul));
+console.log(typeof (nul));
 
 let und = undefined;
-console.log(typeof(und));
+console.log(typeof (und));
 
 
 
 //  Q NUMBER 9
 
 let slice = "javascript is really awesome";
-let slice2 = slice.slice(0 , 20);
-console.log (slice2)
+let slice2 = slice.slice(0, 20);
+console.log(slice2)
 
 
 //  Q NUMBER 10
 
 let name = "tameem";
 let surname = "akhtar";
-console.log (name  + " " +surname);
+console.log(name + " " + surname);
+
+
+
+//  Q NUMBER 11
+ let q11= "good" ;
+ let q122 = "morning";
+ let result =`${q11} ${q122}`;
+ console.log(result);
+
+
+
+ //  Q NUMBER 12m
+
+
+
+ const fvrtNmbr = 26;
+ let multi =fvrtNmbr * 5;
+ console.log (multi);
+ 
+
+
+ //  Q NUMBER 13
+ q13 = " learn javascript  is fun";
+console.log(q13.toUpperCase())
+
+
+
+
+ //  Q NUMBER 14
+q13 = "hello  javascript ";
+console.log(q13.toLowerCase())
+
+
+
+
+ //  Q NUMBER 15
+
+ let nmbr1=12;
+ let nmbr2 = 14;
+ let solution = nmbr1%nmbr2;
+ console.log(solution) ;
+
+ 
+
+
+ //  Q NUMBER 16
+
+
+ let city = "kamptee" ;
+ console.log(city.length)
+
+
+
+ 
+ //  Q NUMBER 17
+
+ let include = "i love to code in javascript";
+ console.log(include.includes("code"));
+
+
+
+
+  //  Q NUMBER 18
+  let schoolname = "MM Rabbani high school and junior college";
+  let print = ' I STUDY IN ' + schoolname;
+  console.log(print);
+ 
+
+
+   //  Q NUMBER 19
+   let myCountry = "brazil";
+   console.log(myCountry.length)
+
+
+    //  Q NUMBER 20
+    let twenty = "coding is cool";
+    console.log(twenty.slice(0,6));
+    

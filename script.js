316f5d8c229d0ae991e2a59e7d3f28434d2554f5
tameem akhtar <akhtartamimm@gmail.com>
2025-10-27 12:38:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+
+//  Q NUMBER 1
+let t = "ilaichi";
+console.log ("my fvrt food is : " + t)
+
+
+//  Q NUMBER 2
+
+const birthYear = 2007;
+const currentYear = 2025;
+const age = currentYear - birthYear;
+console.log(age);
+
+//  Q NUMBER 3
+
+
+let string = "i love javascript programming";
+ let tameem = string.slice(7,18)
+console.log(tameem);
+
+//  Q NUMBER 4
+
+let football = "football is my favourite sports";
+console.log(football)
+ let cricket = football.replace("football", "cricket");
+console.log (cricket);
+
+//  Q NUMBER 5
+
+
+let world = "hello world , world is beatifull";
+console.log (world);
+let replace = world.replaceAll ( "world" , "earth");
+console.log (replace);
+
+
+//  Q NUMBER 6
+
+let fruits = "apple,banana, grapes,mango";
+let fruitss= fruits.split() ;
+console.log(fruitss);
+
+
+//  Q NUMBER 7
+
+let number1 = 99;
+let number2 = 101;
+let adding = (number1+number2);
+console.log(adding);
+
+let minuse = (number1-number2);
+console.log(minuse);
+
+let multiply = (number1*number2);
+console.log(multiply);
+
+let divide = (number1/number2);
+console.log(divide);
+
+
+
+//  Q NUMBER 8
+
+let num = 99;
+console.log(typeof(num));
+
+let str = "tameem";
+console.log(typeof(str));
+
+let bool = true;
+console.log(typeof(bool));
+
+let nul = null;
+console.log(typeof(nul));
+
+let und = undefined;
+console.log(typeof(und));
+
+
+
+//  Q NUMBER 9
+
+let slice = "javascript is really awesome";
+let slice2 = slice.slice(0 , 20);
+console.log (slice2)
+
+
+//  Q NUMBER 10
+
+let name = "tameem";
+let surname = "akhtar";
+console.log (name  + " " +surname);
